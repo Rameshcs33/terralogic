14be070d2de013307514955465a6ad0858d66920
Rameshcs33 <ramesh.s.qodequay@gmail.com>
2025-01-04 16:59:28 +0530

    updated

--- android/app/build/intermediates/cxx/RelWithDebInfo/51ds6134/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\rames\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Ramesh\\terralogic\\android\\app\\.cxx\\RelWithDebInfo\\51ds6134\\arm64-v8a" ^
-  appmodules ^
-  react_codegen_safeareacontext
--- android/app/build/intermediates/cxx/RelWithDebInfo/51ds6134/logs/armeabi-v7a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\rames\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Ramesh\\terralogic\\android\\app\\.cxx\\RelWithDebInfo\\51ds6134\\armeabi-v7a" ^
-  appmodules ^
-  react_codegen_safeareacontext
